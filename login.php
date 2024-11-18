ebff87057885268021f77b8014632c18667cc168
Mateusz0707 <162964775+Mateusz0707@users.noreply.github.com>
2024-11-18 13:44:13 +0100

    Dodanie plikow do logowania i rejestracji

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LOgi</title>
+</head>
+<body>
+    <h2>Logowanie</h2>
+    <form method="POST" action="login.php">
+        <label for="username">Nazwa użytkownika:</label><br>
+        <input type="text" id="username" name="username" required><br><br>
+        <label for="password">Hasło:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+        <button type="submit">Zaloguj</button>
+    </form>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reje</title>
+</head>
+<body>
+    <h2>Rejestracja</h2>
+    <form method="POST" action="register.php">
+        <label for="username">Nazwa użytkownika:</label><br>
+        <input type="text" id="username" name="username" required><br><br>
+        <label for="password">Hasło:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+        <button type="submit">Zarejestruj</button>
+    </form>
+</body>
+</html>
