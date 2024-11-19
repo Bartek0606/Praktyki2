dcb19dfc8905cc087f05d08fb1bff116d3ce6a6d
= <=>
2024-11-19 10:09:05 +0100

    dodanie_navbara_i_slidera

---
+++ glowna.js
@@ -0,0 +1,17 @@
+const slider = document.querySelector(".event-slider");
+const leftBtn = document.querySelector(".left-btn");
+const rightBtn = document.querySelector(".right-btn");
+
+// Funkcja przesuwająca slider
+function scrollSlider(direction) {
+  const scrollAmount = 270; // Ilość przesunięcia
+  if (direction === "left") {
+    slider.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+  } else if (direction === "right") {
+    slider.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  }
+}
+
+// Obsługa kliknięć strzałek
+leftBtn.addEventListener("click", () => scrollSlider("left"));
+rightBtn.addEventListener("click", () => scrollSlider("right"));
