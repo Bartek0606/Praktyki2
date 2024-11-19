71767b56523b7246d68413f70a4cf754425bde5d
Bartek0606 <gnacinskibartek06@gmail.com>
2024-11-19 10:08:01 +0100

    zm

--- register.php
+++ register.php
@@ -1,16 +1,37 @@
 <!DOCTYPE html>
-<html>
+<html lang="pl">
 <head>
-    <title>Reje</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign up • HobbyHub</title>
+    <link rel="stylesheet" href="register.css">
 </head>
 <body>
-    <h2>Rejestracja</h2>
+
+<div id="container">
+    <div id="logo">
+    <h1>HobbyHub</h1>
+    </div>
+    <div id="form_login">
     <form method="POST" action="register.php">
-        <label for="username">Nazwa użytkownika:</label><br>
-        <input type="text" id="username" name="username" required><br><br>
-        <label for="password">Hasło:</label><br>
-        <input type="password" id="password" name="password" required><br><br>
-        <button type="submit">Zarejestruj</button>
+        <input type="text" id="username" name="username" placeholder="Email"  ><br>
+        <input type="text" id="fullname" name="fullname" placeholder="Full Name"  ><br>
+        <input type="text" id="username" name="username" placeholder="Username" ><br>
+        <input type="password" id="password" name="password" placeholder="Password" ><br>
+        <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password" ><br>
+        <button class="button-10" role="button">Sign up</button>
     </form>
+    </div>
+
+
+</div>
+<div id="container_register">
+    <p>Have an account? 
+<a href="login.php">Log in</a>        
+</p>
+</div>
+
+   
+   
 </body>
 </html>
