c6f4afc63331cea3ba11a43a2dc590443c2a8788
Mateusz <06matii@gmail.com>
2024-11-26 11:03:56 +0100

    doanei_navbar

--- profile.php
+++ profile.php
@@ -2,6 +2,14 @@
 session_start(); 
 
 include 'db_connection.php';
+include 'Component/navbar.php';
+
+$isLoggedIn = isset($_SESSION['user_id']);
+
+$userId = $isLoggedIn ? $_SESSION['user_id'] : null;
+$userName = $isLoggedIn ? $_SESSION['username'] : null;
+
+$navbar = new Navbar($conn, $isLoggedIn, $userId, $userName);
 
 $emailError = ''; // Zmienna na komunikat o błędzie e-maila
 $usernameError = ''; // Zmienna na komunikat o błędzie username
@@ -96,36 +104,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
 <header>
-    <nav class="navbar">
-        <div class="logo">
-            <h1><a href="index.php">HobbyHub</a></h1>
-        </div>
-       
-        <div class="navbar-right">
-            <?php if (isset($_SESSION['user_id'])): ?>
-                <?php
-                $sql_image = "SELECT profile_picture FROM users WHERE user_id = '$user_id'";
-                $result_image = $conn->query($sql_image);
-                $image_src = 'default-avatar.jpg'; 
-                if ($result_image->num_rows > 0) {
-                    $row = $result_image->fetch_assoc();
-                    if (!empty($row['profile_picture']) && $row['profile_picture'] !== 'default.png') {
-                        $image_src = 'data:image/jpeg;base64,' . base64_encode($row['profile_picture']);
-                    } else {
-                        $image_src = 'default.png'; 
-                    }
-                }
-                ?>
-                <a href="profile.php" class="profile-link">
-                    <img src="<?php echo $image_src; ?>" alt="Profile Picture" class="navbar-profile-img">
-                    <span class="navbar-username"><?php echo htmlspecialchars($_SESSION['username']); ?></span>
-                </a>
-                <form method="POST" class="logout-form">
-                    <button type="submit" name="logout" class="logout-btn">Log out</button>
-                </form>
-            <?php endif; ?>
-        </div>
-    </nav>
+<?php
+    echo $navbar->render();
+    ?>
 </header>
 <main>
     <!-- Kwadrat: Edycja profilu -->
