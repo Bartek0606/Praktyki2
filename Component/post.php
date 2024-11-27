047b9afd096ff07da402cb1d52c9d599899d4503
Mateusz <06matii@gmail.com>
2024-11-27 11:59:08 +0100

    odanie_dzlalnosci_like

--- Component/post.php
+++ Component/post.php
@@ -3,11 +3,13 @@ class PostRender{
     private $dbConnection;
     private $categoryName;
     private $posts;
+    private $userId;
 
-    public function __construct($dbConnection, $categoryName = '') {
+    public function __construct($dbConnection, $categoryName = null, $userId = null) {
         $this->dbConnection = $dbConnection;
         $this->categoryName = $categoryName;
         $this->posts = $this->fetchPosts();
+        $this->userId = $userId;
     }
     private function fetchPosts() {
         if (!empty($this->categoryName)) {
@@ -34,6 +36,37 @@ class PostRender{
 
         return $this->dbConnection->query($sql);
     }
+    public function like($userId){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['like'])) {
+            //if ($isLoggedIn) {
+                $post_id = $_POST['post_id']; 
+                $sql_check = "SELECT * FROM `user_likes` WHERE user_id = ? AND post_id = ?";
+                $stmt_check = $this->dbConnection->prepare($sql_check);
+                $stmt_check->bind_param("ii", $userId, $post_id);
+                $stmt_check->execute();
+                $result_check = $stmt_check->get_result();
+                if ($result_check->num_rows > 0) {
+                    echo "juz ma ";
+            exit();
+                } else {
+                    $sql_register = "INSERT INTO `user_likes`(`user_id`, `post_id`) VALUES (?, ?)";
+                    $stmt_register = $this->dbConnection->prepare($sql_register);
+                    $stmt_register->bind_param("ii", $userId, $post_id);
+                    $stmt_register->execute();
+                    if ($stmt_register->affected_rows > 0) {
+                        echo "nie ma ";
+                    } else {
+                        echo "There was an error adding your like.";
+                    }
+        
+                }
+            // } else {
+            //     $message = "<p>You must be logged in to register for this event.</p>";
+            // }
+        }
+
+    }
+    
     public function render() {
         ob_start(); 
         ?>
@@ -50,8 +83,11 @@ class PostRender{
                                 <h2><?php echo htmlspecialchars($row['title']); ?></h2>
                                 <p>Category: <?php echo htmlspecialchars($row['category_name']); ?></p>
                                 <p><?php echo $row['content']; ?></p>
-                                <p>Date: <?php echo htmlspecialchars($row['created_at']); ?></p>
-                                <button class="heart"></button>
+                                <p>Date: <?php echo $row['created_at']; ?></p>
+                                <form method="POST" action="">
+                                    <input type="hidden" name="post_id" value="<?php echo $row['post_id']; ?>">
+                                    <button class="heart" name="like"></button>
+                                </form>
                             </div>
                         </div>
                     </a>
