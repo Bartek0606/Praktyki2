f50f64421384e01b458812460a338ef2aa3283e5
Mati <06matii@gmail.com>
2024-12-04 12:55:09 +0100

    dalsza-zmina

--- Component/navbar.php
+++ Component/navbar.php
@@ -52,31 +52,6 @@ class Navbar{
                     <span class="text-2xl font-semibold whitespace-nowrap dark:text-white">HobbyHub</span>
                 </a>
 
-                <button
-                    data-collapse-toggle="navbar-default"
-                    type="button"
-                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="navbar-default"
-                    aria-expanded="false"
-                >
-                    <span class="sr-only">Open main menu</span>
-                    <svg
-                        class="w-5 h-5"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 17 14"
-                    >
-                        <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M1 1h15M1 7h15M1 13h15"
-                        />
-                    </svg>
-                </button>
-
                 <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 p-4 md:p-0 bg-gray-50 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900">
                         <li><a href="index.php" class="block py-2 px-3 text-gray-900 hover:text-blue-700 dark:text-white">Home</a></li>
@@ -88,7 +63,7 @@ class Navbar{
                                 >
                                     Categories
                                 </button>
-                                <div id="dropdownMenu" class="absolute hidden bg-white shadow-lg dark:bg-gray-800">
+                                <div id="dropdownMenu" class="absolute hidden bg-white shadow-lg dark:bg-gray-800 z-20 top-full left-0 w-40">
                                     <?php foreach ($this->categories as $category): ?>
                                         <a href="subpage.php?id=<?php echo $category['category_id']; ?>" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">
                                             <?php echo htmlspecialchars($category['name']); ?>
