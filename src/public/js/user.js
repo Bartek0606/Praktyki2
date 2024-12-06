546358d9e747cfa52a6e6333930b3c340de48d09
Mateusz0707 <matfijalkowski123@gmail.com>
2024-12-06 12:53:48 +0100

    zmiana_tailwind_user

--- src/public/js/user.js
+++ src/public/js/user.js
@@ -2,46 +2,45 @@ document.addEventListener("DOMContentLoaded", function () {
   const postsButton = document.getElementById("show-posts");
   const likesButton = document.getElementById("show-likes");
   const eventsButton = document.getElementById("show-events");
-  const itemsButton = document.getElementById("show-items"); // Nowy przycisk
+  const itemsButton = document.getElementById("show-items");
 
   const postsContainer = document.getElementById("posts-container");
   const likesContainer = document.getElementById("likes-container");
   const eventsContainer = document.getElementById("events-container");
-  const itemsContainer = document.getElementById("items-container"); // Nowy kontener
+  const itemsContainer = document.getElementById("items-container");
+
+  function hideAllContainers() {
+    postsContainer.style.display = "none";
+    likesContainer.style.display = "none";
+    eventsContainer.style.display = "none";
+    itemsContainer.style.display = "none";
+  }
 
   if (postsButton) {
     postsButton.addEventListener("click", function () {
+      hideAllContainers();
       postsContainer.style.display = "block";
-      if (likesContainer) likesContainer.style.display = "none";
-      if (eventsContainer) eventsContainer.style.display = "none";
-      if (itemsContainer) itemsContainer.style.display = "none"; // Ukryj sekcję przedmiotów
     });
   }
 
   if (likesButton) {
     likesButton.addEventListener("click", function () {
-      if (postsContainer) postsContainer.style.display = "none";
+      hideAllContainers();
       likesContainer.style.display = "block";
-      if (eventsContainer) eventsContainer.style.display = "none";
-      if (itemsContainer) itemsContainer.style.display = "none"; // Ukryj sekcję przedmiotów
     });
   }
 
   if (eventsButton) {
     eventsButton.addEventListener("click", function () {
-      if (postsContainer) postsContainer.style.display = "none";
-      if (likesContainer) likesContainer.style.display = "none";
+      hideAllContainers();
       eventsContainer.style.display = "block";
-      if (itemsContainer) itemsContainer.style.display = "none"; // Ukryj sekcję przedmiotów
     });
   }
 
   if (itemsButton) {
     itemsButton.addEventListener("click", function () {
-      if (postsContainer) postsContainer.style.display = "none";
-      if (likesContainer) likesContainer.style.display = "none";
-      if (eventsContainer) eventsContainer.style.display = "none";
-      itemsContainer.style.display = "block"; // Pokaż sekcję przedmiotów
+      hideAllContainers();
+      itemsContainer.style.display = "block";
     });
   }
 });
