2147f29f2a4888278378e59907dc5761d03bd5e0
Mateusz <06matii@gmail.com>
2024-12-10 13:35:57 +0100

    zmina_register

--- src/function.php
+++ src/function.php
@@ -69,5 +69,20 @@ function getUserByEmail($conn, $email) {
 function verifyPassword($inputPassword, $storedPasswordHash) {
     return password_verify($inputPassword, $storedPasswordHash);
 }
+
+function checkUserExists($conn, $email, $username) {
+    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ? OR username = ?");
+    $stmt->bind_param('ss', $email, $username);
+    $stmt->execute();
+    return $stmt->get_result();
+}
+
+function insertUser($conn, $username, $email, $passwordHash, $fullname, $defaultProfilePicture) {
+    $stmt = $conn->prepare("INSERT INTO users (username, email, password_hash, full_name, created_at, profile_picture, is_admin) VALUES (?, ?, ?, ?, NOW(), ?, 0)");
+    $stmt->bind_param('sssss', $username, $email, $passwordHash, $fullname, $defaultProfilePicture);
+    return $stmt->execute();
+}
+
 ?>
+
 
