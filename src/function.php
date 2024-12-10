da7b4bf7102a2b14f16d296db698ce164f882be6
Mateusz <06matii@gmail.com>
2024-12-10 13:27:16 +0100

    login_zmina_struktury

--- src/function.php
+++ src/function.php
@@ -46,10 +46,28 @@ function getMessages($conn, $userId, $profileUserId) {
     $stmt_messages->execute();
     return $stmt_messages->get_result();
 }
+
 function logout() {
     session_start(); 
     session_destroy(); 
     header("Location: index.php"); 
     exit;
 }
+
+function validateEmail($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function getUserByEmail($conn, $email) {
+    $sql = "SELECT * FROM users WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    return $stmt->get_result()->fetch_assoc();
+}
+
+function verifyPassword($inputPassword, $storedPasswordHash) {
+    return password_verify($inputPassword, $storedPasswordHash);
+}
 ?>
+
